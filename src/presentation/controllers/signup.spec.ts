4a758c171c638e9a4120bb60ab962a271ec08329
Davi <al.davigvalerio@gmail.com>
2022-03-26 13:13:26 -0300

    refactor: move a fake httpRequest creation to a factory method

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -2,19 +2,23 @@ import { SignUpController, SignUpDto } from '@/presentation/controllers/signup';
 import { MissingParamError } from '@/presentation/errors/missing-param-error';
 import { HttpRequest } from '@/presentation/protocols/http';
 
+const httpRequestFake = (): HttpRequest<SignUpDto> => ({
+  body: {
+    name: 'any_name',
+    email: 'any_email',
+    password: 'any_password',
+    passwordConfirmation: 'any_passwordConfirmation',
+  },
+});
+
 const makeSut = (): SignUpController => new SignUpController();
 
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
     const sut = makeSut();
+    const httpRequest = httpRequestFake();
 
-    const httpRequest: HttpRequest<SignUpDto> = {
-      body: {
-        email: 'any_email',
-        password: 'any_password',
-        passwordConfirmation: 'any_passwordConfirmation',
-      },
-    };
+    delete httpRequest.body.name;
 
     const httpResponse = sut.handle(httpRequest);
 
@@ -24,14 +28,9 @@ describe('SignUp Controller', () => {
 
   test('Should return 400 if no email is provided', () => {
     const sut = makeSut();
+    const httpRequest = httpRequestFake();
 
-    const httpRequest: HttpRequest<SignUpDto> = {
-      body: {
-        name: 'any_name',
-        password: 'any_password',
-        passwordConfirmation: 'any_passwordConfirmation',
-      },
-    };
+    delete httpRequest.body.email;
 
     const httpResponse = sut.handle(httpRequest);
 
@@ -41,14 +40,9 @@ describe('SignUp Controller', () => {
 
   test('Should return 400 if no password is provided', () => {
     const sut = makeSut();
+    const httpRequest = httpRequestFake();
 
-    const httpRequest: HttpRequest<SignUpDto> = {
-      body: {
-        name: 'any_name',
-        email: 'any_name',
-        passwordConfirmation: 'any_passwordConfirmation',
-      },
-    };
+    delete httpRequest.body.password;
 
     const httpResponse = sut.handle(httpRequest);
 
@@ -58,14 +52,9 @@ describe('SignUp Controller', () => {
 
   test('Should return 400 if no password confirmation is provided', () => {
     const sut = makeSut();
+    const httpRequest = httpRequestFake();
 
-    const httpRequest: HttpRequest<SignUpDto> = {
-      body: {
-        name: 'any_name',
-        email: 'any_name',
-        password: 'any_password',
-      },
-    };
+    delete httpRequest.body.passwordConfirmation;
 
     const httpResponse = sut.handle(httpRequest);
 
