6333ebc5e0bf540e569abceebd9cb1eec8721457
Davi <al.davigvalerio@gmail.com>
2022-05-02 23:47:27 -0300

    refactor: group validation imports in a single file

---
+++ src/presentation/helpers/validators/index.ts
@@ -0,0 +1,4 @@
+export * from '@/presentation/helpers/validators/compare-fields-validation';
+export * from '@/presentation/helpers/validators/email-validation';
+export * from '@/presentation/helpers/validators/required-field-validation';
+export * from '@/presentation/helpers/validators/validation-composite';
